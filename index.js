00916b35ead80a291b94ecfea2fd1c47864258d9
gabumwaingo <gabumwaingo@gmail.com>
2024-05-04 07:12:45 +0300

    finished up the course

---
+++ index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const body = document.querySelector(".body");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("nav--visible");
+  menu.classList.add("menu--hidden");
+  close.classList.add("close--visible");
+  body.classList.add("body--no-scroll");
+});
+close.addEventListener("click", () => {
+  nav.classList.remove("nav--visible");
+  menu.classList.remove("menu--hidden");
+  close.classList.remove("close--visible");
+  body.classList.remove("body--no-scroll");
+});
